9779d22703a38de11b803392373d7ad879bf34d0
Viraj Dhumal <mevirajdhumal@gmail.com>
2022-03-07 20:03:37 +0530

    Added description for if-else and an example. Added IfElse link in ConceptOverview.html

--- concepts/IfElse.php
+++ concepts/IfElse.php
@@ -1,1 +1,41 @@
-// Place if else explaination here and an example
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concept | If else</title>
+</head>
+<body>
+
+    <?php
+    // Place if else explaination here and an example
+
+    /**
+     * If else statement works on a condition.
+     * If a condition is true then the 'if' block is executed.
+     * If the condition is false, the program searches for nearest else block, if present.
+     * 
+     * Else cannot exist it-self, there has to be atleast one if-statement.
+     * 
+     * Syntax:
+     * if(condition) {
+     *      //code
+     * } else {
+     *      //code
+     * }
+     */
+
+    $number = 10;
+
+    // If number is even
+    if ($number % 2 == 0) {
+        echo "The number $number is Even.";
+    } else {
+        echo "The number $number is Odd.";
+    }
+    
+
+    ?>
+</body>
+</html>
