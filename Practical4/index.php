003cccbf42ccb6f5a7a7dd1517ae5a1c011e11fc
Viraj Dhumal <mevirajdhumal@gmail.com>
2022-04-12 20:13:22 +0530

    Added user input feature
    Now user can enter his/her own values to find factorial.

--- Practical4/index.php
+++ Practical4/index.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practical 5 | Factorial of number</title>
 </head>
+
 <body>
     <h1>Factorial of a number.</h1>
+    <form action="" method="post">
+        <label>Enter a number:</label>
+        <input type="text" name="input" />
+        <input type="submit" name="submit" value="Submit" /><br /><br />
+    </form>
     <?php
 
-    $n = 5;
-    $ans = 1;
-    for ($i=2; $i <= $n; $i++) { 
-        $ans = $ans * $i;
+    if (isset($_POST['submit'])) {
+        $n = $_POST['input'];
+        $ans = 1;
+        for ($i = 2; $i <= $n; $i++) {
+            $ans = $ans * $i;
+        }
+    
+        echo "Factorial of $n is $ans <br>";
     }
-
-    echo "Factorial of $n is $ans <br>";
     ?>
 </body>
+
 </html>
